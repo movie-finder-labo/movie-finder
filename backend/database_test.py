617b1aa54d62af4f5efca1596c4d230b25e602ff
VoidPunz <andersseberg@gmail.com>
2025-10-02 13:58:27 +0200

    Refactored MovieFinderDB to be asynchronous, dropped pymongo_flask module as it's redundant

--- backend/database_test.py
+++ backend/database_test.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from dotenv import dotenv_values
 from moviefinder.database.mongo import MovieFinderDB
 import pymongo
+import asyncio
 
-app = Flask("TEST")
-try:
-    global db
-    db = MovieFinderDB(app)
-except pymongo.errors.ServerSelectionTimeoutError as err:
-    print(f"Failed to connect: {err}")
-except Exception as ex:
-    print(f"Unexpected error: {ex}")
-    
-print(db.GetUserByUsername("") is None)
+dbConfig = dotenv_values()
+
+async def Test():
+    try:
+        db = MovieFinderDB(connectionString=dbConfig["MONGO_URI"], databaseName=dbConfig["MONGO_DATABASE"])
+        await db.Connect()
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        print(f"Failed to connect: {err}")
+    except Exception as ex:
+        print(f"Unexpected error: {ex}")
+        
+    print(await db.GetUserByUsername("lol"))
+
+asyncio.run(Test())
