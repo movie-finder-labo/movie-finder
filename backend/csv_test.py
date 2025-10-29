a8868f3b9c61115f476d379f74cfad09b40b9c85
VoidPunz <andersseberg@gmail.com>
2025-10-29 01:06:57 +0100

    Almost finished

--- backend/csv_test.py
+++ backend/csv_test.py
@@ -1,25 +1,26 @@
-from backend.moviefinder.csv import DeserializeCSV
-import os
+from backend.moviefinder.csv import DeserializeCSV, GetDataFiles
+from os import getcwd
 from time import time
 
-def TestSerializeDirectory(path: str):
-    absPath = os.getcwd() + path
+def TestDeserializePath(path: str):
+    absPath = getcwd() + path
     print(f"Serialising csv file at directory {absPath}:")
     clock = time()
     try:
         data = DeserializeCSV(path)
-    except Exception:
-        print("Deserialization failed.")
-    clock = time() - clock # Don't waste time counting the object with len first
-    print(f"[{len(data)}] Time elapsed for file at path {absPath}: {clock} seconds.{data[0]}\n")
+        clock = time() - clock # Don't waste time counting the object with len first
+        print(f"[{len(data)}] Time elapsed for file at path {absPath}: {clock} seconds.\n")
+    except Exception as e:
+        print(f"Deserialization failed: {e}.")
+    
+def TestDeserializeDirectory(path: str):
+    for fp in GetDataFiles():
+        TestDeserializePath(fp)
 
 if __name__ == "__main__":
     try:
-        print(f"Current working directory: {os.getcwd()}.\nBeginning deserialization...")
-        TestSerializeDirectory("ml-32m/tags.csv")
-        TestSerializeDirectory("ml-32m/links.csv")
-        TestSerializeDirectory("ml-32m/movies.csv")
-        TestSerializeDirectory("ml-32m/ratings.csv")
+        print(f"Current working directory: {getcwd()}.\nBeginning deserialization...")
+        TestDeserializeDirectory("data")
     except KeyboardInterrupt: # CTRL + C
         print("Keyboard interrupt.")
     finally:
