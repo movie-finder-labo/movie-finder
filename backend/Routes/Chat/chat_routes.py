76c57a4514acd3a8431ef7bc4f82d750b95b0949
NSpahic22 <113034921+NSpahic22@users.noreply.github.com>
2025-10-31 14:47:31 +0100

    Made main work again

--- backend/Routes/Chat/chat_routes.py
+++ backend/Routes/Chat/chat_routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, Blueprint
-from services.openai_service import ask_openai
-from backend.moviefinder.csv import InitializeMovieData
+from backend.services.openai_service import ask_openai
+from backend.backend.moviefinder.csv import InitializeMovieData
 
 chat_bp = Blueprint("chat", __name__)
 
